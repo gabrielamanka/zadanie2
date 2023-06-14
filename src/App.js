bfdb0c4d972ac08b7be49ab01f4d1e23f7fc456f
gabrielamanka <133596439+gabrielamanka@users.noreply.github.com>
2023-06-14 20:56:47 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Gabriela Mańka <code>src/App.js</code> and save to reload.
+          Gabriela Mańka <code>src/App.js</code>
         </p>
         <a
           className="App-link"
